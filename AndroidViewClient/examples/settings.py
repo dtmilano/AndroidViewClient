12c3860757be0d1e230a85504fdd1f5a8afdedad
Diego Torres Milano <dtmilano@gmail.com>
2012-02-02 01:08:48 -0500

    Initial commit

---
+++ AndroidViewClient/examples/settings.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env monkeyrunner
+'''
+Created on Feb 1, 2012
+
+@author: diego
+'''
+
+
+import re
+import sys
+
+# this must be imported before MonkeyRunner and MonkeyDevice,
+# otherwise the import fails
+sys.path.append('/Users/diego/Work/workspace/AndroidViewClient/src')
+from com.dtmilano.android.viewclient import ViewClient
+
+from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
+
+package='com.android.settings'                                          
+activity='.Settings'                           
+component_name=package + "/" + activity                        
+device = MonkeyRunner.waitForConnection(60)
+if True:
+    device.startActivity(component=component_name)
+    MonkeyRunner.sleep(3)
+    device.press('KEYCODE_DPAD_DOWN') # extra Visual Multi-tasking setting WARNING!
+    MonkeyRunner.sleep(1)
+    device.press('KEYCODE_DPAD_CENTER', "DOWN_AND_UP")
+    device.press('KEYCODE_DPAD_DOWN', MonkeyDevice.DOWN_AND_UP)
+    #device.press('KEYCODE_DPAD_DOWN', MonkeyDevice.DOWN_AND_UP)
+    #device.press('KEYCODE_DPAD_DOWN', MonkeyDevice.DOWN_AND_UP)
+    #device.press('KEYCODE_DPAD_CENTER', "DOWN_AND_UP")
+    #device.press('KEYCODE_DPAD_CENTER', "DOWN_AND_UP")
+
+
+vc = ViewClient(device)
+regex = "id/checkbox.*"
+p = re.compile(regex)
+vc.dump()
+found = False
+for id in vc.getViewIds():
+    #print id
+    m = p.match(id)
+    if m:
+        found = True
+        attrs =  vc.findViewById(id)
+        if attrs['isSelected()'] == 'true':
+            print "Wi-Fi is",
+            if attrs['isChecked()'] != 'true':
+                print "not",
+            print "set"
+
+if not found:
+    print "No Views found that match " + regex
---
+++ AndroidViewClient/src/com/dtmilano/android/viewclient.py
@@ -0,0 +1,113 @@
+'''
+Created on Feb 2, 2012
+
+@author: diego
+'''
+
+import unittest
+import subprocess
+import re
+import socket
+import os
+
+DEBUG = False
+
+ANDROID_HOME = os.environ['ANDROID_HOME'] if os.environ.has_key('ANDROID_HOME') else '/Users/diego/opt/android-sdk'
+VIEW_SERVER_HOST = 'localhost'
+VIEW_SERVER_PORT = 4939
+
+class ViewClient:
+    '''
+    ViewClient is a ViewServer client.
+    
+    If not running the ViewServer is started on the target device or emulator and then the port
+    mapping is created.
+    '''
+
+    def __init__(self, device):
+        if not self.serviceResponse(device.shell('service call window 3')):
+            self.assertServiceResponse(device.shell('service call window 1 i32 %d' %
+                VIEW_SERVER_PORT))
+
+        subprocess.check_call([ANDROID_HOME + '/platform-tools/adb',
+            'forward', 'tcp:%d' % VIEW_SERVER_PORT, 'tcp:%d' % VIEW_SERVER_PORT])
+
+        self.viewsById = {}
+    
+    def assertServiceResponse(self, response):
+        unittest.assertTrue(self.serviceResponse(response))
+
+    def serviceResponse(self, response):
+        return response == "Result: Parcel(00000000 00000001   '........')\r\n"
+
+    def dump(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((VIEW_SERVER_HOST, VIEW_SERVER_PORT))
+        s.send('dump -1\r\n')
+        received = ""
+        doneRE = re.compile("DONE")
+        while True:
+            received += s.recv(1024)
+            if doneRE.search(received[-7:]):
+                break
+
+        s.close()
+        self.views = received.split("\n")
+        if DEBUG:
+            print "there are %d views in this dump" % len(self.views)
+
+        idRE = re.compile("(?P<viewId>id/\S+)")
+        attrRE = re.compile("(?P<attr>\S+)(\(\))?=\d+,(?P<val>\S+)")
+        hashRE = re.compile("(?P<class>\S+)@(?P<oid>[0-9a-f]+)")
+
+        for v in self.views:
+            attrs = {}
+            m = idRE.search(v)
+            if m:
+                viewId = m.group('viewId')
+                if DEBUG:
+                    print "found %s" % viewId
+                for attr in v.split():
+                    m = attrRE.match(attr)
+                    if m:
+                        attrs[m.group('attr')] = m.group('val')                    
+                    else:
+                        m = hashRE.match(attr)
+                        if m:
+                            attrs['class'] = m.group('class')
+                            attrs['oid'] = m.group('oid')
+                        else:
+                            if DEBUG:
+                                print attr, "doesn't match"
+
+                    
+                if viewId in self.viewsById:
+                    i = 1
+                    while True:
+                        newId = viewId + '/%d' % i
+                        if not newId in self.viewsById:
+                            break
+                        i += 1
+                    viewId = newId
+                if DEBUG:
+                    print "adding viewById %s" % viewId
+                self.viewsById[viewId] = attrs
+
+        return self.views
+
+    def findViewById(self, viewId):
+        '''
+        Finds the View with the specified viewId.
+        '''
+        return self.viewsById[viewId]
+
+    def getViewIds(self):
+        '''
+        Returns the Views map.
+        '''
+        return self.viewsById
+
+
+
+if __name__ == "__main__":
+    ViewClient(None)
