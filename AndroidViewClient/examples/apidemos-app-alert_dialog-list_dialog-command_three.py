cb1909d7459778dbee3545d76212a7f9d48bb72a
Diego Torres Milano <dtmilano@gmail.com>
2013-09-21 16:22:23 -0400

    examples: updated apidemos-*

--- AndroidViewClient/examples/apidemos-app-alert_dialog-list_dialog-command_three.py
+++ AndroidViewClient/examples/apidemos-app-alert_dialog-list_dialog-command_three.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env monkeyrunner
+#! /usr/bin/env python
 '''
 Copyright (C) 2012  Diego Torres Milano
 Created on Sep 5, 2012
@@ -11,23 +11,12 @@ import re
 import sys
 import os
 
-# This must be imported before MonkeyRunner and MonkeyDevice,
-# otherwise the import fails.
-# PyDev sets PYTHONPATH, use it
-try:
-    for p in os.environ['PYTHONPATH'].split(':'):
-       if not p in sys.path:
-          sys.path.append(p)
-except:
-    pass
-    
 try:
     sys.path.append(os.path.join(os.environ['ANDROID_VIEW_CLIENT_HOME'], 'src'))
 except:
     pass
 
 from com.dtmilano.android.viewclient import ViewClient, View
-from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 
 device, serialno = ViewClient.connectToDeviceOrExit()
 
@@ -36,31 +25,31 @@ FLAG_ACTIVITY_NEW_TASK = 0x10000000
 componentName = 'com.example.android.apis/.ApiDemos'
 device.startActivity(component=componentName, flags=FLAG_ACTIVITY_NEW_TASK)
 
-MonkeyRunner.sleep(3)
+ViewClient.sleep(3)
 vc = ViewClient(device=device, serialno=serialno)
 app = vc.findViewWithText('App')
 if app:
    app.touch()
-   MonkeyRunner.sleep(3)
+   ViewClient.sleep(3)
    # windows changed, request a new dump
    vc.dump()
    ad = vc.findViewWithText('Alert Dialogs')
    if ad:
       ad.touch()
-      MonkeyRunner.sleep(3)
+      ViewClient.sleep(3)
       # windows changed, request a new dump
       vc.dump()
       ld = vc.findViewWithText('List dialog')
       if ld:
          ld.touch()
-         MonkeyRunner.sleep(3)
+         ViewClient.sleep(3)
          # windows changed, request a new dump
          vc.dump()
          c3 = vc.findViewWithText('Command three')
          if c3:
             c3.touch()
-            MonkeyRunner.sleep(10)
-            device.press('KEYCODE_BACK', MonkeyDevice.DOWN_AND_UP)
+            ViewClient.sleep(10)
+            device.press('KEYCODE_BACK')
          else:
             print >> sys.stderr, "Cannot find 'Command three'"
       else:
--- AndroidViewClient/examples/apidemos-preference-advanced_preferences.py
+++ AndroidViewClient/examples/apidemos-preference-advanced_preferences.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env monkeyrunner
+#! /usr/bin/env python
 '''
 Copyright (C) 2012  Diego Torres Milano
 Created on Sep 18, 2012
@@ -11,16 +11,6 @@ import re
 import sys
 import os
 
-# This must be imported before MonkeyRunner and MonkeyDevice,
-# otherwise the import fails.
-# PyDev sets PYTHONPATH, use it
-try:
-    for p in os.environ['PYTHONPATH'].split(':'):
-       if not p in sys.path:
-          sys.path.append(p)
-except:
-    pass
-    
 try:
     sys.path.append(os.path.join(os.environ['ANDROID_VIEW_CLIENT_HOME'], 'src'))
 except:
@@ -30,7 +20,6 @@ import com.dtmilano.android.viewclient as viewclient
 if viewclient.__version__ < '1.0':
     print >> sys.stderr, "%s: This script requires viewclient 1.0 or greater." % os.path.basename(sys.argv[0])
     sys.exit(1)
-from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 
 device, serialno = viewclient.ViewClient.connectToDeviceOrExit()
 
@@ -39,16 +28,20 @@ FLAG_ACTIVITY_NEW_TASK = 0x10000000
 componentName = 'com.example.android.apis/.ApiDemos'
 device.startActivity(component=componentName, flags=FLAG_ACTIVITY_NEW_TASK)
 
-MonkeyRunner.sleep(3)
+viewclient.ViewClient.sleep(3)
 vc = viewclient.ViewClient(device=device, serialno=serialno)
 vc.findViewWithTextOrRaise('Preference').touch()
 vc.dump()
 vc.findViewWithTextOrRaise(re.compile('.*Advanced preferences')).touch()
 vc.dump()
-value0 = vc.findViewByIdOrRaise('id/mypreference_widget').getText()
 myPreference = vc.findViewWithTextOrRaise('My preference')
+if vc.getSdkVersion() >= 16:
+    _id = 'id/no_id/22'
+else:
+    _id = 'id/mypreference_widget'
+value0 = vc.findViewByIdOrRaise(_id).getText()
 for i in range(10):
     myPreference.touch()
 vc.dump()
-value1 = vc.findViewByIdOrRaise('id/mypreference_widget').getText()
+value1 = vc.findViewByIdOrRaise(_id).getText()
 print "My preference started with value %s and is now %s" % (value0, value1)
