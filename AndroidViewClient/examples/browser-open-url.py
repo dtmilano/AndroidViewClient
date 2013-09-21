2762ebf1ded3ba85b727fbeae4e99d527e5bb7e3
Diego Torres Milano <dtmilano@gmail.com>
2013-09-21 16:55:11 -0400

    examples: updated browser-open-url.py

--- AndroidViewClient/examples/browser-open-url.py
+++ AndroidViewClient/examples/browser-open-url.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env monkeyrunner
+#! /usr/bin/env python
 '''
 Copyright (C) 2012  Diego Torres Milano
 Created on Mar 13, 2012
@@ -12,37 +12,37 @@ import sys
 import os
 import string
 
-# This must be imported before MonkeyRunner and MonkeyDevice,
-# otherwise the import fails.
-# PyDev sets PYTHONPATH, use it
-try:
-    for p in os.environ['PYTHONPATH'].split(':'):
-        if not p in sys.path:
-            sys.path.append(p)
-except:
-    pass
-    
 try:
     sys.path.append(os.path.join(os.environ['ANDROID_VIEW_CLIENT_HOME'], 'src'))
 except:
     pass
 
 from com.dtmilano.android.viewclient import ViewClient
-from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 
+USE_BROWSER = True
 # Starting: Intent { act=android.intent.action.MAIN flg=0x10000000 cmp=com.android.browser/.BrowserActivity }
-package = 'com.android.browser'
-activity = '.BrowserActivity'
+if USE_BROWSER:
+    package = 'com.android.browser'
+    activity = '.BrowserActivity'
+else:
+    package = 'com.android.chrome'
+    activity = 'com.google.android.apps.chrome.Main'
 component = package + "/" + activity
 uri = 'http://dtmilano.blogspot.com'
 
 device, serialno = ViewClient.connectToDeviceOrExit()
 device.startActivity(component=component, uri=uri)
-MonkeyRunner.sleep(3)
+ViewClient.sleep(5)
 
 vc = ViewClient(device, serialno)
-url = vc.findViewByIdOrRaise("id/url").getText()
-if string.find(url, uri) != -1:
+if vc.getSdkVersion() >= 16:
+    if USE_BROWSER:
+        url = vc.findViewByIdOrRaise("id/no_id/12").getText()
+    else:
+        url = vc.findViewWithContentDescription("Search or type url").getText()
+else:
+    url = vc.findViewByIdOrRaise("id/url").getText()
+if string.find(uri, url) != -1:
     print "%s successfully loaded" % uri
 else:
     print "%s was not loaded, url=%s" % (uri, url)
