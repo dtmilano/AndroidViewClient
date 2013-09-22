04fe94c911153f9a472236d68e92c5f9b6a0093c
Diego Torres Milano <dtmilano@gmail.com>
2013-09-21 22:24:19 -0400

    examples: updated for current version

--- AndroidViewClient/examples/development-settings-show-running-processes.py
+++ AndroidViewClient/examples/development-settings-show-running-processes.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env monkeyrunner
+#! /usr/bin/env python
 '''
 Copyright (C) 2012  Diego Torres Milano
 Created on Feb 3, 2012
@@ -11,44 +11,38 @@ import re
 import sys
 import os
 
-# this must be imported before MonkeyRunner and MonkeyDevice,
-# otherwise the import fails
+
 try:
-    ANDROID_VIEW_CLIENT_HOME = os.environ['ANDROID_VIEW_CLIENT_HOME']
-except KeyError:
-    print >>sys.stderr, "%s: ERROR: ANDROID_VIEW_CLIENT_HOME not set in environment" % __file__
-    sys.exit(1)
-sys.path.append(ANDROID_VIEW_CLIENT_HOME + '/src')
+    sys.path.append(os.path.join(os.environ['ANDROID_VIEW_CLIENT_HOME'], 'src'))
+except:
+    pass
+
 from com.dtmilano.android.viewclient import ViewClient
-
-from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
-
 
 # 01-04 18:23:42.000: I/ActivityManager(4288): Displayed com.android.development/.DevelopmentSettings: +379ms
 package = 'com.android.development'                                          
 activity = '.DevelopmentSettings'                           
-componentName = package + "/" + activity                        
-device = MonkeyRunner.waitForConnection(60, "emulator-5554")
-if not device:
-	raise Exception('Cannot connect to device')
+component = package + "/" + activity                        
+device, serialno = ViewClient.connectToDeviceOrExit()
+device.startActivity(component=component)
+ViewClient.sleep(5)
 
-device.startActivity(component=componentName)
-MonkeyRunner.sleep(5)
+vc = ViewClient(device, serialno)
 
-vc = ViewClient(device)
-vc.dump()
-
-showCpu = vc.findViewById("id/show_cpu")
-showLoad = vc.findViewById("id/show_load")
-alwaysFinish = vc.findViewById("id/always_finish")
+showCpu = vc.findViewWithTextOrRaise("Show CPU usage")
+showLoad = vc.findViewWithTextOrRaise("Show running processes")
+alwaysFinish = vc.findViewWithTextOrRaise("Immediately destroy activities")
 
 if not showLoad.isChecked():
+    print "touching @", showLoad.getCenter()
     showLoad.touch()
 
 if not alwaysFinish.isChecked():
+    print "touching @", alwaysFinish.getCenter()
     alwaysFinish.touch()
 
 if not showCpu.isChecked():
     # WARNING: Show CPU usage is de-activated as soon as it's activated, that's why it seems it
     # is never set
+    print "touching @", showCpu.getCenter()
     showCpu.touch()
