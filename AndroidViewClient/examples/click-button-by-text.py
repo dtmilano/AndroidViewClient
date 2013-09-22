fed5dcb3a2ad3469e539a61e77c517be85863f44
Diego Torres Milano <dtmilano@gmail.com>
2013-09-21 21:52:57 -0400

    examples: updated for current version

--- AndroidViewClient/examples/click-button-by-text.py
+++ AndroidViewClient/examples/click-button-by-text.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env monkeyrunner
+#! /usr/bin/env python
 '''
 Copyright (C) 2012  Diego Torres Milano
 Created on May 5, 2012
@@ -10,16 +10,6 @@ import sys
 import os
 import time
 
-# This must be imported before MonkeyRunner and MonkeyDevice,
-# otherwise the import fails.
-# PyDev sets PYTHONPATH, use it
-try:
-    for p in os.environ['PYTHONPATH'].split(':'):
-        if not p in sys.path:
-            sys.path.append(p)
-except:
-    pass
-    
 try:
     sys.path.append(os.path.join(os.environ['ANDROID_VIEW_CLIENT_HOME'], 'src'))
 except:
