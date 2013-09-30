78e0e4b502f6e876b77e681cf739c56a433ac7e2
Diego Torres Milano <dtmilano@gmail.com>
2013-09-30 00:05:01 -0400

    Added setup.py

---
+++ AndroidViewClient/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='AndroidViewClient',
+    version='4.4.2',
+    description='''AndroidViewClient is a 100% pure python tool that
+        simplifies test script creation providing higher level operations and the ability of
+        obtaining the tree of Views present at any given moment on the device or emulator screen.
+        ''',
+    license='Apache',
+    keywords='android uiautomator viewclient monkeyrunner',
+    author='Diego Torres Milano',
+    author_email='dtmilano@gmail.com',
+    url='https://github.com/dtmilano/AndroidViewClient/',
+    package_dir={'com.dtmilano.android':'src'},
+    packages=['com.dtmilano.android', 'tests.com.dtmilano.android'],
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: Apache Software License']
+    )
