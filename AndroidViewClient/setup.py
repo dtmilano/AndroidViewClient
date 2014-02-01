8efa4ec2e72f3f17200ab530420ddd4694648b91
Diego Torres Milano <dtmilano@gmail.com>
2014-01-31 23:18:07 -0500

    culebra: fixed indentation of generated script

--- AndroidViewClient/setup.py
+++ AndroidViewClient/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='androidviewclient',
-    version='4.10.0',
+    version='4.10.1',
     description='''AndroidViewClient is a 100% pure python tool that
         simplifies test script creation providing higher level operations and the ability of
         obtaining the tree of Views present at any given moment on the device or emulator screen.
