76421293ed3f69a9fcb6cdd91ed61bb9a1f65a4e
Diego Torres Milano <dtmilano@gmail.com>
2014-04-16 14:51:03 -0400

    culebra: corrected indentation of some scripts
     - Version 5.4.4

--- AndroidViewClient/setup.py
+++ AndroidViewClient/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='androidviewclient',
-    version='5.4.3',
+    version='5.4.4',
     description='''AndroidViewClient is a 100% pure python tool that
         simplifies test script creation providing higher level operations and the ability of
         obtaining the tree of Views present at any given moment on the device or emulator screen.
