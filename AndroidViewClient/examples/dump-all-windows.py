c758f2e5a601ad0de6202347af04808778a40ce5
Diego Torres Milano <dtmilano@gmail.com>
2014-04-26 01:04:20 -0400

    Added windows dump example

---
+++ AndroidViewClient/examples/dump-all-windows.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+'''
+Copyright (C) 2014  Diego Torres Milano
+Created on Apr 24, 2014
+
+@author: diego
+'''
+
+from com.dtmilano.android.viewclient import ViewClient
+
+kwargs1 = {'verbose': True, 'ignoresecuredevice': True}
+kwargs2 = {'startviewserver': True, 'forceviewserveruse': True, 'autodump': False, 'ignoreuiautomatorkilled': True}
+vc = ViewClient(*ViewClient.connectToDeviceOrExit(**kwargs1), **kwargs2)
+windows = vc.list()
+for wId in windows.keys():
+    print ">>> window=", wId, windows[wId]
+    vc.dump(window=wId)
+    vc.traverse(transform=ViewClient.TRAVERSE_CIT, indent="    ")
