fc881af24f01054de32404ed4ccb2caad91f7559
Diego Torres Milano <dtmilano@gmail.com>
2014-05-24 00:04:52 -0400

    Added screenshot options to dump
    - Version 6.1.0
    - Corrected short options

--- AndroidViewClient/setup.py
+++ AndroidViewClient/setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='androidviewclient',
-    version='6.0.0',
+    version='6.1.0',
     description='''AndroidViewClient is a 100% pure python tool that
         simplifies test script creation providing higher level operations and the ability of
         obtaining the tree of Views present at any given moment on the device or emulator screen.
