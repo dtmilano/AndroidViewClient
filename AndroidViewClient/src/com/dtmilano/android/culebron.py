bc65cd1d453779c8e8e5c37ad8458a13665150e0
Diego Torres Milano <dtmilano@gmail.com>
2014-10-08 23:39:34 -0400

    Added culebron main methods
     - Screenshot
     - Click handler
     - Key handler

--- AndroidViewClient/src/com/dtmilano/android/culebron.py
+++ AndroidViewClient/src/com/dtmilano/android/culebron.py
@@ -20,6 +20,8 @@ limitations under the License.
 
 __version__ = '8.0.4'
 
+import sys
+
 try:
     from PIL import Image, ImageTk
     PIL_AVAILABLE = True
@@ -36,8 +38,19 @@ except:
 
 from ast import literal_eval as make_tuple
 
+DEBUG = True
+DEBUG_MOVE = DEBUG and False
+DEBUG_TOUCH = DEBUG and False
+DEBUG_POINT = DEBUG and True
+DEBUG_KEY = DEBUG and True
 
 class Culebron:
+    canvas = None
+    imageId = None
+    vignetteId = None
+    areTargetsMarked = False
+    isGrabbing = False
+    
     @staticmethod
     def checkDependencies():
         if not PIL_AVAILABLE:
@@ -45,11 +58,76 @@ class Culebron:
         if not TKINTER_AVAILABLE:
             raise Exception("Tkinter is needed for GUI mode")
 
-    def __init__(self):
-        pass
+    def __init__(self, vc):
+        self.vc = vc
+        self.device = vc.device
+        self.serialno = vc.serialno
+        self.window = Tkinter.Tk()
 
     def takeScreenshotAndShowItOnWindow(self):
+        image = self.device.takeSnapshot(reconnect=True)
+        # FIXME: allow scaling
+        (width, height) = image.size
+        if self.canvas is None:
+            if DEBUG:
+                print >> sys.stderr, "Creating canvas", width, 'x', height
+            self.canvas = Tkinter.Canvas(self.window, width=width, height=height)
+            self.canvas.focus_set()
+            self.enableEvents()
+            self.createVignette(width, height)
+        if DEBUG:
+            print >> sys.stderr, "    canvas=", self.canvas
+        self.screenshot = ImageTk.PhotoImage(image)
+        if self.imageId is not None:
+            self.canvas.delete(self.imageId)
+        self.imageId = self.canvas.create_image(0, 0, anchor=Tkinter.NW, image=self.screenshot)
+        try:
+            self.canvas.pack_info()
+        except:
+            self.canvas.pack(fill=Tkinter.BOTH)
+        self.findTargets()
+        self.hideVignette()
+        
+    def createVignette(self, width, height):
         pass
+    
+    def showVignette(self):
+        pass
+    
+    def hideVignette(self):
+        pass
+    
+    def findTargets(self):
+        pass
+    
+    def getViewContainingPointAndTouch(self, x, y):
+        if DEBUG:
+            print >> sys.stderr, 'getViewContainingPointAndTouch(%d, %d)' % (x, y)
+        pass
+    
+    def button1Pressed(self, event):
+        self.getViewContainingPointAndTouch(event.x, event.y)
+        
+    def keyPressed(self):
+        pass
+    
+    def ctrlS(self):
+        pass
+    
+    def enableEvents(self):
+        self.canvas.update_idletasks()
+        self.canvas.bind("<Button-1>", self.button1Pressed)
+        self.canvas.bind("<BackSpace>", self.keyPressed)
+        self.canvas.bind("<Control-Key-S>", self.ctrlS)
+        self.canvas.bind("<Key>", self.keyPressed)
+        self.areEventsDisabled = False
+
+    def disableEvents(self):
+        self.canvas.update_idletasks()
+        self.areEventsDisabled = True
+        self.canvas.unbind("<Button-1>")
+        self.canvas.unbind("<BackSpace>")
+        self.canvas.unbind("<Key>")
 
     def mainloop(self):
-        pass
+        self.window.mainloop()
