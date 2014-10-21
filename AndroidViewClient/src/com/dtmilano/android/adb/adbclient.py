0ee04cb0b3193dd830c443ff99cff90bafddeb8a
Diego Torres Milano <dtmilano@gmail.com>
2014-10-21 14:01:37 -0400

    Escape text in asserts
     - Version 8.3.3

--- AndroidViewClient/src/com/dtmilano/android/adb/adbclient.py
+++ AndroidViewClient/src/com/dtmilano/android/adb/adbclient.py
@@ -17,7 +17,7 @@ limitations under the License.
 @author: Diego Torres Milano
 '''
 
-__version__ = '8.3.2'
+__version__ = '8.3.3'
 
 import sys
 import warnings
--- AndroidViewClient/src/com/dtmilano/android/viewclient.py
+++ AndroidViewClient/src/com/dtmilano/android/viewclient.py
@@ -18,7 +18,7 @@ limitations under the License.
 @author: Diego Torres Milano
 '''
 
-__version__ = '8.3.2'
+__version__ = '8.3.3'
 
 import sys
 import warnings
