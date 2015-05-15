f88c2dc645cc16797d6f716b769d4358797d7898
Diego Torres Milano <dtmilano@gmail.com>
2015-05-15 12:19:24 -0400

    Bump
     - Version 1.5.0

--- src/com/dtmilano/android/adb/adbclient.py
+++ src/com/dtmilano/android/adb/adbclient.py
@@ -17,7 +17,7 @@ limitations under the License.
 @author: Diego Torres Milano
 '''
 
-__version__ = '10.4.0'
+__version__ = '10.5.0'
 
 import sys
 import warnings
--- src/com/dtmilano/android/controlpanel.py
+++ src/com/dtmilano/android/controlpanel.py
@@ -19,7 +19,7 @@
     @author: Ahmed Kasem
     '''
 
-__version__ = '10.4.0'
+__version__ = '10.5.0'
 
 import sys, os
 import Tkinter, tkFileDialog, ttk
