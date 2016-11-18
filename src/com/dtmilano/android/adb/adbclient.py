4978bf1eceb32cb287a337764a942c6808483161
Diego Torres Milano <dtmilano@gmail.com>
2016-11-18 13:47:02 -0500

    Bump
     - Version 12.4.2

--- src/com/dtmilano/android/adb/adbclient.py
+++ src/com/dtmilano/android/adb/adbclient.py
@@ -18,7 +18,7 @@ limitations under the License.
 '''
 import threading
 
-__version__ = '12.4.1'
+__version__ = '12.4.2'
 
 import sys
 import warnings
--- src/com/dtmilano/android/viewclient.py
+++ src/com/dtmilano/android/viewclient.py
@@ -18,7 +18,7 @@ limitations under the License.
 @author: Diego Torres Milano
 '''
 
-__version__ = '12.4.1'
+__version__ = '12.4.2'
 
 import sys
 import warnings
