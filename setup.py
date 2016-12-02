951c6222301131a4eae9372ffb7b1864720648cc
Diego Torres Milano <dtmilano@gmail.com>
2016-12-02 12:38:24 -0500

    Bump
     - Version 12.5.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='androidviewclient',
-    version='12.5.0',
+    version='12.5.1',
     description='''AndroidViewClient is a 100% pure python library and tools
         that simplifies test script creation providing higher level
         operations and the ability of obtaining the tree of Views present at
--- src/com/dtmilano/android/adb/adbclient.py
+++ src/com/dtmilano/android/adb/adbclient.py
@@ -18,7 +18,7 @@ limitations under the License.
 '''
 import threading
 
-__version__ = '12.5.0'
+__version__ = '12.5.1'
 
 import sys
 import warnings
