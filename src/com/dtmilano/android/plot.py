a350adea9c5b985c6d7eaff635a46425f3b0ea4b
Diego Torres Milano <dtmilano@gmail.com>
2017-03-13 00:53:14 -0400

    Added multiple axes for plots

--- src/com/dtmilano/android/plot.py
+++ src/com/dtmilano/android/plot.py
@@ -21,22 +21,24 @@ limitations under the License.
 import types
 
 import matplotlib.pyplot as plt
+import mpl_toolkits.axisartist as AA
+import numpy as np
+from mpl_toolkits.axes_grid1 import host_subplot
 
 from com.dtmilano.android.adb.dumpsys import Dumpsys
 
 __version__ = '13.1.0'
 
-
 NumberTypes = (types.IntType, types.LongType, types.FloatType)
 
 
 class Plot:
-
     def __init__(self):
         self.n = 0
         self.na = []
         self.va = []
         self.ava = {}
+        ''' Associative values array '''
 
     def append(self, value):
         self.n += 1
@@ -47,7 +49,7 @@ class Plot:
             dumpsys = value
             if not self.ava:
                 self.__initAva()
-            self.ava[Dumpsys.TOTAL].append(dumpsys.get(Dumpsys.TOTAL)/(1024*1024))
+            self.ava[Dumpsys.TOTAL].append(dumpsys.get(Dumpsys.TOTAL))
             self.ava[Dumpsys.ACTIVITIES].append(dumpsys.get(Dumpsys.ACTIVITIES))
             self.ava[Dumpsys.VIEWS].append(dumpsys.get(Dumpsys.VIEWS))
             self.ava[Dumpsys.VIEW_ROOT_IMPL].append(dumpsys.get(Dumpsys.VIEW_ROOT_IMPL))
@@ -59,16 +61,39 @@ class Plot:
         self.ava[Dumpsys.VIEW_ROOT_IMPL] = []
 
     def plot(self):
-        plt.xlabel('N')
-        plt.ylabel('V')
         if self.ava:
+            host = host_subplot(111, axes_class=AA.Axes)
+            plt.subplots_adjust(right=0.75)
+            axis = 1
             for k in self.ava.keys():
-                plt.plot(self.na, self.ava[k], label=k)
+                if k == Dumpsys.TOTAL:
+                    host.plot(self.na, self.ava[k], label=k)
+                    host.set_xlabel('N')
+                    host.set_ylabel(k)
+                    host.set_xlim(np.amin(self.na), np.amax(self.na))
+                    host.set_ylim(np.amin(self.ava[k]), np.amax(self.ava[k]))
+                else:
+                    par = host.twinx()
+                    offset = axis * 60
+                    axis += 1
+                    new_fixed_axis = par.get_grid_helper().new_fixed_axis
+                    par.axis["right"] = new_fixed_axis(loc="right",
+                                                       axes=par,
+                                                       offset=(offset, 0))
+                    par.axis["right"].toggle(all=True)
+                    par.plot(self.na, self.ava[k], label=k)
+                    par.set_ylabel(k)
+                    par.set_xlim(np.amin(self.na), np.amax(self.na))
+                    par.set_ylim(np.amin(self.ava[k]), np.amax(self.ava[k]))
+            host.legend()
         elif self.va:
+            plt.xlabel('N')
+            plt.ylabel('V')
             plt.plot(self.na, self.va, label="A")
         else:
             raise RuntimeError("No values to plot")
         plt.title('About as simple as it gets, folks')
         plt.grid(True)
-        plt.savefig("plot.png")
+        plt.draw()
+        # plt.savefig("plot.png")
         plt.show()
--- tests/com/dtmilano/android/plottests.py
+++ tests/com/dtmilano/android/plottests.py
@@ -8,6 +8,7 @@ from com.dtmilano.android.adb.dumpsys import Dumpsys
 from com.dtmilano.android.plot import Plot
 
 SERIALNO = '.*'
+SAMPLE_PROCESS_NAME = 'com.android.systemui'
 
 
 class PlotTests(unittest.TestCase):
@@ -20,10 +21,24 @@ class PlotTests(unittest.TestCase):
         self.plot = Plot()
 
     def test_plot_dumpsys_meminfo(self):
-        for n in range(100):
-            self.plot.append(Dumpsys(self.device, Dumpsys.MEMINFO, 'com.android.systemui'))
+        for n in range(10):
+            self.plot.append(Dumpsys(self.device, Dumpsys.MEMINFO, SAMPLE_PROCESS_NAME))
             time.sleep(1)
         self.plot.plot()
+
+    def test_plot_dumpsys_meminfo_culebratester(self):
+        for n in range(10):
+            self.device.startActivity("com.dtmilano.android.sampleapplication/.MainActivity")
+            time.sleep(2)
+            self.plot.append(Dumpsys(self.device, Dumpsys.MEMINFO, "com.dtmilano.android.sampleapplication"))
+            self.device.press('BACK')
+            time.sleep(0.5)
+            self.device.press('BACK')
+            time.sleep(0.5)
+            self.device.press('HOME')
+            time.sleep(0.5)
+        self.plot.plot()
+
 
 
 if __name__ == '__main__':
