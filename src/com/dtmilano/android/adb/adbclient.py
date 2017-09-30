2a7b882dd832864fbe7fb52895fa21b448d4f727
Diego Torres Milano <dtmilano@gmail.com>
2017-09-29 23:45:36 -0400

    Bump
     - Version 13.5.0

--- src/com/dtmilano/android/adb/adbclient.py
+++ src/com/dtmilano/android/adb/adbclient.py
@@ -22,7 +22,7 @@ import unicodedata
 
 from com.dtmilano.android.adb.dumpsys import Dumpsys
 
-__version__ = '13.4.1'
+__version__ = '13.5.0'
 
 import sys
 import warnings
--- src/com/dtmilano/android/uiautomator/uiautomatorhelper.py
+++ src/com/dtmilano/android/uiautomator/uiautomatorhelper.py
@@ -18,7 +18,7 @@ limitations under the License.
 @author: Diego Torres Milano
 '''
 
-__version__ = '13.4.1'
+__version__ = '13.5.0'
 
 import json
 import os
