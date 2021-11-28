6b1623add308112a8f4b263201b28c5ce12dd80c
Diego Torres Milano <dtmilano@gmail.com>
2021-11-28 11:51:55 -0800

    Bump
    - Version 20.4.3

--- src/com/dtmilano/android/adb/adbclient.py
+++ src/com/dtmilano/android/adb/adbclient.py
@@ -26,7 +26,7 @@ import unicodedata
 
 from com.dtmilano.android.adb.dumpsys import Dumpsys
 
-__version__ = '20.4.1'
+__version__ = '20.4.3'
 
 import sys
 import warnings
--- src/com/dtmilano/android/controlpanel.py
+++ src/com/dtmilano/android/controlpanel.py
@@ -20,7 +20,7 @@
     '''
 import platform
 
-__version__ = '20.4.1'
+__version__ = '20.4.3'
 
 import tkinter
 import tkinter.ttk
