d70511c95717d6e940a5d32d00a87a8324840b20
Diego Torres Milano <dtmilano@gmail.com>
2022-06-24 23:26:00 -0700

    Bump
    - Version 21.11.1

--- src/com/dtmilano/android/viewclient.py
+++ src/com/dtmilano/android/viewclient.py
@@ -26,7 +26,7 @@ from typing import Optional
 import culebratester_client
 from culebratester_client import WindowHierarchyChild, WindowHierarchy
 
-__version__ = '21.11.0'
+__version__ = '21.11.1'
 
 import sys
 import warnings
