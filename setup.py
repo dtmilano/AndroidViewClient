cf929f4dec1a13feae85fbcc671da753d8372975
Diego Torres Milano <dtmilano@gmail.com>
2022-07-02 19:29:37 -0700

    Bump
    - Version 21.14.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='androidviewclient',
-      version='21.14.0',
+      version='21.14.1',
       description='''AndroidViewClient is a 100% pure python library and tools
         that simplifies test script creation providing higher level
         operations and the ability of obtaining the tree of Views present at
