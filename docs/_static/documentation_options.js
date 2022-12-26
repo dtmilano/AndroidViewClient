a93e453c6adc5ec0db83788f9709d83ca92a1732
Diego Torres Milano <dtmilano@gmail.com>
2022-12-25 22:00:59 -0800

    Update docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '22.2.0',
+    VERSION: '22.5.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
